3c0ad97aac2a6e744dd4808014404ffd1ffbb4a4
sin <sin@DESKTOP-FPNU47D>
2019-08-28 15:05:35 +0900

    1

--- src/main/java/com/example/demo/KOCompanyApplication.java
+++ src/main/java/com/example/demo/KOCompanyApplication.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -33,26 +31,26 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 public class KOCompanyApplication implements CommandLineRunner{
-	
+
 	@Autowired
 	private KotraMapper kotraMapper;
-	
+
 	private static KotraMapper mapper;
 
 	@PostConstruct
 	public void setKotraMapper() {
 		this.mapper = this.kotraMapper;
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(KOCompanyApplication.class, args);
 		long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
-		
+
 		//static String cvsfile ="D:\\작업폴더\\KOTRA\\크롤링자료\\inter_co_sample4.csv";
 //		String csvfile ="D:\\작업폴더\\KOTRA\\크롤링자료\\url\\해외기업.csv";
 		String csvfile ="D:\\작업폴더\\KOTRA\\크롤링자료\\url\\국내기업.csv";
 		File filename = new File(csvfile);
-		
+
 		if(csvfile.contains("국내")) {
 			log.debug("======> 국내기업 크롤링 및 번역 시작.");
 			domestic(filename,1);
@@ -62,26 +60,26 @@ public class KOCompanyApplication implements CommandLineRunner{
 		}else {
 			log.debug("======> csv 파일명을 확인하세요.");
 		}
-		
-		
-		
-		
+
+
+
+
         long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
         long secDiffTime = (afterTime - beforeTime)/1000; //두 시간에 차 계산
         log.debug("시간차이 : "+secDiffTime+"(초)");
-		
+
 	}
 
-	
 
-	
+
+
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	/** 해외기업크롤링 번역
 	 * @param fileName
 	 * @param startLine
@@ -90,7 +88,7 @@ public class KOCompanyApplication implements CommandLineRunner{
 		log.debug("============> 해외기업 시작라인 : "+ startLine);
 		int cutlen = 5500;
 		int lineNum = 0;
-		
+
 	    List<Company> data = CSVReadInternational(fileName);
 	    log.debug("============> 해외기업 전체 건수:"+data.size());
 	    Iterator<Company> it = data.iterator();
@@ -106,7 +104,7 @@ public class KOCompanyApplication implements CommandLineRunner{
 	        		com.setOrgText(bodyText);
 	        		//log.debug("bodyText:"+bodyText);
 	        		//번역
-	        		
+
 	        		TransResult tr = Translator.subStrByteAndTranslate(bodyText, cutlen);
 	        		//log.debug(tr.toString());
 	        		if(tr != null) {
@@ -115,16 +113,16 @@ public class KOCompanyApplication implements CommandLineRunner{
 	        			com.setByteLength(tr.getByteLength());
 	        		}
 	        	}
-	            
+
 	        	com.setLineNum(lineNum);
 	        	//log.debug("get=====>"+mapper.get().toString());
 	        	log.debug(com.toString());
 	        	mapper.addInternationalCompany(com);
-	            
+
 	    	}
 	    }
 	}
-	
+
 	/** 국내기업 크롤링 번역
 	 * @param fileName
 	 * @param startLine
@@ -133,7 +131,7 @@ public class KOCompanyApplication implements CommandLineRunner{
 		log.debug("============> 국내기업 시작라인 : "+ startLine);
 		int cutlen = 5500;
 		int lineNum = 0;
-		
+
 	    List<Company> data = CSVReadDomestic(fileName);
 	    log.debug("============> 국내기업 전체 건수:"+data.size());
 	    Iterator<Company> it = data.iterator();
@@ -149,7 +147,7 @@ public class KOCompanyApplication implements CommandLineRunner{
 	        		com.setOrgText(bodyText);
 	        		//log.debug("bodyText:"+bodyText);
 	        		//번역
-	        		
+
 	        		TransResult tr = Translator.subStrByteAndTranslate(bodyText, cutlen);
 	        		//log.debug(tr.toString());
 	        		if(tr != null) {
@@ -158,17 +156,17 @@ public class KOCompanyApplication implements CommandLineRunner{
 	        			com.setByteLength(tr.getByteLength());
 	        		}
 	        	}
-	            
+
 	        	com.setLineNum(lineNum);
 	        	//log.debug("get=====>"+mapper.get().toString());
 	        	log.debug(com.toString());
 	        	mapper.addDomesticCompany(com);
-	            
+
 	    	}
 	    }
-		
+
 	}
-	
+
 	/** 국내 csv파일 일기
 	 * @param filename
 	 * @return
@@ -216,17 +214,17 @@ public class KOCompanyApplication implements CommandLineRunner{
         }
 		return data;
 	}
-	
-	
-	
+
+
+
 	/** URL을 받아 body text 크롤링
 	 * @param webPage
 	 * @return
 	 */
 	private static String jsoupBody(String webPage) {
-		
+
 		//String webPage = "http://www.aspic.co.kr";//en
-        
+
         //webPage = "http://www.hdlift.co.kr";
 		String bodyText = null;
         try {
@@ -234,14 +232,14 @@ public class KOCompanyApplication implements CommandLineRunner{
         	//get input stream from the URL
 //            InputStream inStream = new URL(webPage).openStream();
 //            Document document = Jsoup.parse(inStream, "UTF-8", webPage);
-        	
+
 			Document document = Jsoup.connect(webPage).get();
 			//System.out.printf("Html: %s%n", html2);
 			bodyText = document.select("body").text();
-			
+
 			//log.debug("Language:"+detectLanguage(bodyText));
 			//System.out.printf("Body: %s", bodyText);
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -249,9 +247,9 @@ public class KOCompanyApplication implements CommandLineRunner{
 
         return bodyText;
 	}
-	
-	
-	
+
+
+
 	/*
 	private static List<String []> CSVRead(File filename) {
 		List<String[]> data = new ArrayList<String[]>();
@@ -272,10 +270,10 @@ public class KOCompanyApplication implements CommandLineRunner{
 		return data;
 	}
 	*/
-	
-	
-	
-	
-	
+
+
+
+
+
 
 }
